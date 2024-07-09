616830c62554bdb08c023b217def5c7ec2e42e61
Pedro <pedro2007.prrr@gmail.com>
2024-07-09 14:50:45 -0300

    Cake now has a default component

--- src/main/java/net/pedroricardo/item/PBItems.java
+++ src/main/java/net/pedroricardo/item/PBItems.java
@@ -7,9 +7,12 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.pedroricardo.PedrosBakery;
 import net.pedroricardo.block.PBBlocks;
+import net.pedroricardo.block.helpers.CakeBatter;
+
+import java.util.Collections;
 
 public class PBItems {
-    public static final Item CAKE = register("cake", new PBCakeBlockItem(PBBlocks.CAKE, new Item.Settings()));
+    public static final Item CAKE = register("cake", new PBCakeBlockItem(PBBlocks.CAKE, new Item.Settings().component(PBComponentTypes.BATTER_LIST, Collections.singletonList(CakeBatter.getDefault().withBakeTime(2000)))));
     public static final Item FROSTING_BOTTLE = register("frosting_bottle", new FrostingBottleItem(new Item.Settings().maxCount(1).recipeRemainder(Items.GLASS_BOTTLE)));
 
     public static Item register(String id, Item item) {
