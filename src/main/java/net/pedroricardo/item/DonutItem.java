0d6f88b41129f133f4786be8b8dd91e6de080f89
Pedro <pedro2007.prrr@gmail.com>
2024-07-21 17:26:08 -0300

    Added donut crafting recipes

--- src/main/java/net/pedroricardo/item/DonutItem.java
+++ src/main/java/net/pedroricardo/item/DonutItem.java
@@ -1,10 +1,17 @@
 package net.pedroricardo.item;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 import net.pedroricardo.block.helpers.CakeTop;
+import net.pedroricardo.block.tags.PBTags;
 
 import java.util.List;
 
@@ -19,6 +26,16 @@ public class DonutItem extends Item {
         if (top == null) {
             return;
         }
-        tooltip.add(Text.translatable("item.pedrosbakery.donut.top", Text.translatable(top.getTranslationKey())));
+        tooltip.add(Text.translatable("item.pedrosbakery.donut.top", Text.translatable(top.getTranslationKey())).formatted(Formatting.GRAY));
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+        CakeTop top = stack.get(PBComponentTypes.TOP);
+        if (top != null && top.isIn(PBTags.Tops.INEDIBLE)) {
+            return TypedActionResult.fail(stack);
+        }
+        return super.use(world, user, hand);
     }
 }
---
+++ src/main/java/net/pedroricardo/item/recipes/FrostedDonutRecipe.java
@@ -0,0 +1,78 @@
+package net.pedroricardo.item.recipes;
+
+import com.google.common.collect.Lists;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.SpecialCraftingRecipe;
+import net.minecraft.recipe.book.CraftingRecipeCategory;
+import net.minecraft.recipe.input.CraftingRecipeInput;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.world.World;
+import net.pedroricardo.PedrosBakery;
+import net.pedroricardo.block.PBBlocks;
+import net.pedroricardo.item.FrostingBottleItem;
+import net.pedroricardo.item.PBComponentTypes;
+import net.pedroricardo.item.PBItems;
+
+import java.util.ArrayList;
+
+public class FrostedDonutRecipe extends SpecialCraftingRecipe {
+    public FrostedDonutRecipe(CraftingRecipeCategory category) {
+        super(category);
+    }
+
+    @Override
+    public boolean matches(CraftingRecipeInput inventory, World world) {
+        ItemStack donutStack = ItemStack.EMPTY;
+        ItemStack bottleStack = ItemStack.EMPTY;
+        for (int i = 0; i < inventory.getSize(); ++i) {
+            ItemStack itemStack2 = inventory.getStackInSlot(i);
+            if (itemStack2.isEmpty()) continue;
+            if (itemStack2.isOf(PBItems.DONUT)) {
+                if (!donutStack.isEmpty()) {
+                    return false;
+                }
+                donutStack = itemStack2;
+                continue;
+            }
+            if (itemStack2.getItem() instanceof FrostingBottleItem && itemStack2.contains(PBComponentTypes.TOP)) {
+                if (!bottleStack.isEmpty()) {
+                    return false;
+                }
+                bottleStack = itemStack2;
+                continue;
+            }
+            return false;
+        }
+        return !donutStack.isEmpty() && !bottleStack.isEmpty();
+    }
+
+    @Override
+    public ItemStack craft(CraftingRecipeInput inventory, RegistryWrapper.WrapperLookup lookup) {
+        ItemStack donutStack = ItemStack.EMPTY;
+        ItemStack bottleStack = ItemStack.EMPTY;
+        for (int i = 0; i < inventory.getSize(); ++i) {
+            ItemStack itemStack2 = inventory.getStackInSlot(i);
+            if (itemStack2.isEmpty()) continue;
+            if (itemStack2.isOf(PBItems.DONUT)) {
+                donutStack = itemStack2;
+            } else if (itemStack2.getItem() instanceof FrostingBottleItem && itemStack2.contains(PBComponentTypes.TOP)) {
+                bottleStack = itemStack2;
+            }
+        }
+        ItemStack newStack = donutStack.copyWithCount(1);
+        newStack.set(PBComponentTypes.TOP, bottleStack.get(PBComponentTypes.TOP));
+        return newStack;
+    }
+
+    @Override
+    public boolean fits(int width, int height) {
+        return width * height >= 2;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return PBRecipeSerializers.FROSTED_DONUT;
+    }
+}
