00a86d55175c11cfa701c0b553960b5e2cca408f
Pedro <pedro2007.prrr@gmail.com>
2024-12-17 21:26:06 -0300

    Fix TOGGLE_COOKIE_SHAPE in place of SET_COOKIE_SHAPE

--- src/client/java/net/pedroricardo/network/PBClientNetworkRegistry.java
+++ src/client/java/net/pedroricardo/network/PBClientNetworkRegistry.java
@@ -11,7 +11,7 @@ import java.util.Set;
 
 public class PBClientNetworkRegistry {
     public static void init() {
-        ClientPlayNetworking.registerGlobalReceiver(PBNetworkRegistry.TOGGLE_COOKIE_SHAPE, (client, handler, buf, responseSender) -> {
+        ClientPlayNetworking.registerGlobalReceiver(PBNetworkRegistry.SET_COOKIE_SHAPE, (client, handler, buf, responseSender) -> {
             if (client.player != null && client.player.currentScreenHandler instanceof CookieTableScreenHandler cookieTable) {
                 Set<Vector2i> set = buf.decodeAsJson(PBCodecs.VECTOR_2I.listOf().xmap(HashSet::new, ArrayList::new));
                 cookieTable.setCookieShape(set);
