3dee3f072f9eb9bd9439d5df2d3a87169261970b
Pedro <pedro2007.prrr@gmail.com>
2024-12-27 14:31:14 -0300

    Add background to cookie table screen

--- src/client/java/net/pedroricardo/screen/CookieTableScreen.java
+++ src/client/java/net/pedroricardo/screen/CookieTableScreen.java
@@ -48,6 +48,7 @@ public class CookieTableScreen extends HandledScreen<CookieTableScreenHandler> {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderBackground(context);
         super.render(context, mouseX, mouseY, delta);
         this.drawMouseoverTooltip(context, mouseX, mouseY);
     }
